5954e24ebd8d4ff2f83604f61b4a38ed0247a395
patrick archer <archer.patrick@gmail.com>
2022-09-14 16:32:29 -0400

    initial commit

---
+++ importValidationData.py
@@ -0,0 +1,132 @@
+import json
+
+import csv
+import importlib
+import pandas as pd
+import numpy as np
+import os
+import hashlib
+import decimal
+import re
+import redis
+import logging
+
+import pymysql
+import rds_config
+
+bb2_host = rds_config.bb2_host
+bb2_username = rds_config.bb2_username
+bb2_password = rds_config.bb2_password
+bb2_dbname = rds_config.bb2_dbname
+
+
+# mysql -hbb2-dev.cluster-chyay8gb54w8.us-west-1.rds.amazonaws.com -uadmin_bb2 -p3pX45n9MIGCm
+
+
+def lambda_handler(event, context):
+    # TODO implement
+
+    try:
+
+        conn = pymysql.connect(host=bb2_host, user=bb2_username, passwd=bb2_password, db=bb2_dbname, connect_timeout=5,
+                               cursorclass=pymysql.cursors.DictCursor)
+    except pymysql.MySQLError as e:
+        logger.error("ERROR: Unexpected error: Could not connect to MySQL instance.")
+        logger.error(e)
+        sys.exit()
+
+    redisPass1 = 'f^@vLY^ao3gXlPl7'
+    redisPass2 = 'tgOee0%^6GgqneO3'
+
+    r = redis.Redis(host='clustercfg.bb2.fh5ygv.memorydb.us-west-1.amazonaws.com', port=6379, username='bb2-lambda',
+                    password='f^@vLY^ao3gXlPl7', db=0)
+
+    # clustercfg.bb2.fh5ygv.memorydb.us-west-1.amazonaws.com:6379
+
+    pipe = r.pipeline()
+    # print(event['Records'])
+    # print(event['Records'])
+
+    # cur = conn.cursor(pymysql.cursors.DictCursor)
+    cur = conn.cursor()
+
+    # print(cur)
+
+    cur.execute(rds_config.sqls['getDbCreds'])
+
+    dbList = cur.fetchall()
+
+    # print(dbList)
+
+    for dbCreds in dbList:
+
+        try:
+            dbConn = pymysql.connect(host=dbCreds["host"], user=dbCreds["db_user"], passwd=dbCreds["db_pass"],
+                                     db=dbCreds["db_name"], connect_timeout=5,
+                                     cursorclass=pymysql.cursors.DictCursor)
+            # print(dbConn)
+        except pymysql.MySQLError as e:
+            # logger.error("ERROR: Unexpected error: Could not connect to MySQL instance.")
+            # logger.error(e)
+            print(e)
+            print(dbCreds)
+
+        currentConn = dbConn.cursor()
+
+        for i in rds_config.sqls:
+            dataToInsert = {}
+            print(i)
+            if i == 'getDbCreds':
+                print("skipping " + i)
+                continue
+
+            currentConn.execute(rds_config.sqls[i])
+
+            results = currentConn.fetchall();
+            print(results)
+            for row in results:
+                # print("key vals: " )
+                # print(dbCreds["abbv"])
+                # print(i)
+                # print(row)
+                key = i + ":<>:" + dbCreds["abbv"] + ":<>:" + (getKey(i, row))
+
+                # if key not in dataToInsert:
+                #     dataToInsert[key] = []
+
+                # dataToInsert[key].append(getVal(i, row))
+                pipe.set(key, json.dumps(getVal(i, row)))
+                print(r.get(key))
+
+        pipe.execute()
+        # print(dataToInsert)
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps('validation tables done')
+    }
+
+
+def getKey(type, row):
+    # print("key type: " + type)
+
+    key = row["phone"]
+
+    if type == "dist":
+        key = row["account_no"] + ":<>:" + row["phone"]
+
+    # if there is no match for type, return phone
+    return key
+
+
+def getVal(type, row):
+    # print("val type: " + type)
+    switcher = {
+        "dist": "customer_id",
+        "product": "product_id",
+        # "cust": row["account_no"],
+    }
+    # print("val swutcher selected: "+ switcher.get(type))
+
+    # if there is no match for type, return account_no
+    return row[switcher.get(type, "account_no")]
---
+++ rds_config.py
@@ -0,0 +1,44 @@
+
+bb2_host = "bb2-dev.cluster-chyay8gb54w8.us-west-1.rds.amazonaws.com"
+bb2_username = "admin_bb2"
+bb2_password = "3pX45n9MIGCm"
+bb2_dbname = "bigblue-2"
+
+#mysql -hbb2-dev.cluster-chyay8gb54w8.us-west-1.rds.amazonaws.com -uadmin_bb2 -p3pX45n9MIGCm
+
+# conn = pymysql.connect(host="bb2-dev.cluster-chyay8gb54w8.us-west-1.rds.amazonaws.com", user="admin_bb2", passwd="3pX45n9MIGCm", db="bigblue-2", connect_timeout=5,
+#                 cursorclass=pymysql.cursors.DictCursor)
+
+sqls = {
+    "getDbCreds": """select cb.* from  accounts a join client_databases cb on cb.id=a.db_connection where cb.abbv <> '' limit 1; """,
+    "dist": """select distinct
+        map.distributor_seller_account as account_no,
+        p.phone as phone,
+        if(map.customer_id is not null,map.customer_id,if(map.distributor_purchaser_id is NULL,1041779,map.distributor_purchaser_id)) as customer_id
+        from
+        customer_distributor_map map
+        join master_distributor m on m.master_abv=map.master_abv
+        join distributor_phones p on p.master_distributor_id=m.id;""",
+        # where
+        # map.distributor_seller_account in (%s) and p.phone in (%s);""",
+
+    "product": """select
+        distinct
+        map.upc_code as upc_code,
+        p.phone as phone,
+        map.distributor_product_id as product_id
+        from
+        product_distributor_map map
+        join master_distributor m on m.master_abv=map.master_abv
+        join distributor_phones p on p.master_distributor_id=m.id;""",
+        # where p.phone = %s;""",
+
+    "cust": """select distinct
+        map.distributor_seller_account as account_no, p.phone
+        from
+        customer_distributor_map map
+        join master_distributor m on m.master_abv=map.master_abv
+        join distributor_phones p on p.master_distributor_id=m.id;""",
+        # where p.phone = %s """,
+    # "custValidationSql2": """ and map.distributor_seller_account in (%s);"""
+}
